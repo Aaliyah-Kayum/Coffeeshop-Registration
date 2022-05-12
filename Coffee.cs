3c401a2455f1964dd994dbd55e9f7ffcdaf410e6
Aaliyah Kayum <kayumaaliyah@gmail.com>
2022-05-12 15:56:19 -0400

    coffee

---
+++ Coffee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MVCIntro.Models
+{
+    public class Coffee
+    {
+        [Key]
+        public int Id { get; set; } 
+        [StringLength(25)]
+        public string Name { get; set; }
+        [StringLength(25)]
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Type { get; set; }
+    }
+}
